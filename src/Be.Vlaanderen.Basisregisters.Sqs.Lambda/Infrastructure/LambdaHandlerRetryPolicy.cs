875cd18ddf6a16fae2c72bc3933c20aa9eda4792
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-10-14 10:50:33 +0200

    feat: add lambda code

---
+++ src/Be.Vlaanderen.Basisregisters.Sqs.Lambda/Infrastructure/LambdaHandlerRetryPolicy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Polly;
+using Polly.Retry;
+
+namespace Be.Vlaanderen.Basisregisters.Sqs.Lambda.Infrastructure
+{
+    public sealed class LambdaHandlerRetryPolicy : ICustomRetryPolicy
+    {
+        private readonly AsyncRetryPolicy _policy;
+
+        public LambdaHandlerRetryPolicy(int maxRetryCount, int delaySeconds)
+        {
+            _policy = Policy
+                .Handle<SqlException>(ex => TransientSqlErrors.Errors.Contains(ex.Number))
+                .WaitAndRetryAsync(
+                    maxRetryCount,
+                    currentRetryCount => TimeSpan
+                        .FromSeconds(delaySeconds)
+                        .Multiply(currentRetryCount));
+        }
+
+        public async Task Retry(Func<Task> functionToRetry)
+        {
+            await _policy.ExecuteAsync(functionToRetry);
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.Sqs.Lambda/Requests/SqsLambdaRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+using MediatR;
+
+namespace Be.Vlaanderen.Basisregisters.Sqs.Lambda.Requests
+{
+    using System.Collections.Generic;
+
+    public class SqsLambdaRequest : IRequest
+    {
+        public Guid TicketId { get; set; }
+        public string MessageGroupId { get; set; }
+        public string? IfMatchHeaderValue { get; set; }
+        public Provenance Provenance { get; set; }
+        public IDictionary<string, object> Metadata { get; set; }
+    }
+}
