92cf49f8c988a3f67b34e1c66c06e43b15a1d59f
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-10-14 10:50:04 +0200

    fix: add IfMatchHeaderValueMismatchException

---
+++ src/Be.Vlaanderen.Basisregisters.Sqs/Exceptions/IfMatchHeaderValueMismatchException.cs
@@ -0,0 +1,20 @@
+﻿namespace Be.Vlaanderen.Basisregisters.Sqs.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public sealed class IfMatchHeaderValueMismatchException : Exception
+    {
+        public IfMatchHeaderValueMismatchException()
+        { }
+
+        public IfMatchHeaderValueMismatchException(string? message)
+            : base(message)
+        { }
+
+        private IfMatchHeaderValueMismatchException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
