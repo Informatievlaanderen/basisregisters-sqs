adc24eb8080fa582d284d8f5464bb6607e7e5cb8
Arne Dumarey <dumarey.arne@gmail.com>
2023-09-04 10:46:14 +0200

    chore: add assembly info to packages

---
+++ src/Be.Vlaanderen.Basisregisters.Sqs.Lambda/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("Basisregisters Vlaanderen AWS SQS Lambda common handlers")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("6456c898-6576-4a78-9bfc-fa11e513f323")]
